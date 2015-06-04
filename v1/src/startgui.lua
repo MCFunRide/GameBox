9fd76d842399d9e6e168156580074b0ab7d296a8
MCFunRide <funridedoesminecraft@gmail.com>
2015-06-04 16:24:52 +0100

    Create startgui.lua

---
+++ v1/src/startgui.lua
@@ -0,0 +1,35 @@
+os.loadAPI("/apis/graphix")
+term.setTextColor(colors.white)
+graphix.bgColor(colors.blue)
+term.setCursorPos(1,1)
+term.setBackgroundColor(colors.lightBlue)
+term.clearLine()
+term.setCursorPos(1,19)
+term.clearLine()
+graphix.centerWrite(19,"GameBox by Aperture")
+term.setCursorPos(1,1)
+graphix.centerWrite(1,"GameBox by Aperture")
+term.setCursorPos(1,3)
+term.setBackgroundColor(colors.green)
+graphix.center(3,"Games")
+graphix.center(5,"Floppy Games")
+graphix.center(7,"About")
+graphix.center(9,"Power")
+while true do
+  local evt, button, x, y = os.pullEvent('mouse_click')
+  if y == 3 then
+    shell.run("games")
+  elseif y == 5 then
+    if fs.exists("/disk/.game") then
+      shell.run("/disk/.game")
+    else
+      shell.run("/startgui")
+    end
+  elseif y == 7 then
+    shell.run("about")
+  elseif y == 9 then
+    shell.run("power")
+  else
+    shell.run("startgui")
+  end
+end
